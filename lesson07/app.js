511d427d99392bdb4cede8ef5a254c7db823a77a
volosgoto <volosgoto@yahoo.com>
2019-05-02 11:04:00 +0300

    ditertives

---
+++ lesson07/app.js
@@ -0,0 +1,10 @@
+new Vue({
+  el: "#app",
+  data: {
+    src: "https://picsum.photos/id/237/200/300",
+    alt: "dog",
+    link: "http://google.com",
+    myText: "Hello text",
+    myHtml: "<strong>Fransis Jones</strong>"
+  }
+});
