3f18888e2fa281c9d0fab95caeddcb273953dacd
andrey <volosgoto@yahoo.com>
2019-05-04 15:05:03 +0300

    for

---
+++ lesson09/app.js
@@ -0,0 +1,18 @@
+new Vue({
+  el: "#app",
+  data: {
+    names: ["Sara", "John", "Mike"],
+    cars: [
+      { color: "black", model: "Ford" },
+      { color: "yellow", model: "Seat" },
+      { color: "green", model: "BMW" },
+      { color: "red", model: "Ferrari" }
+    ],
+    user: {
+      name: "Sara",
+      lastname: "Manson",
+      age: 25
+    }
+  },
+  methods: {}
+});
