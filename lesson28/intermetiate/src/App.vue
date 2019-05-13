2724352a1cd3bb915512baf7c63a324ad50be738
volosgoto <volosgoto@yahoo.com>
2019-05-13 11:15:22 +0300

    registering a components

--- lesson28/intermetiate/src/App.vue
+++ lesson28/intermetiate/src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
+    <app-header></app-header>
     <div>Hello {{name}}</div>
     <div>Hello to all</div>
+    <componentFooter></componentFooter>
+
   </div>
 </template>
 
 <script>
+// Local usage of conponent
+import componentFooter from './Components/Header_footer/Footer'
 export default {
   data(){
     return {
       name: 'Sara'
     }
+  },
+  // Register components
+  components: {
+    // "app-footer": Footer
+    componentFooter
+    
   }
 }
 </script>
--- lesson28/intermetiate/src/main.js
+++ lesson28/intermetiate/src/main.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+// Include components by global
+import Header from "./Components/Header_footer/Header.vue";
+Vue.component("app-header", Header);
+// Include components by global
+
 new Vue({
   el: "#app",
   render: h => h(App)
