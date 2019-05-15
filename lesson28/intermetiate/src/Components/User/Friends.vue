d4ccc13518e1ac8d49575ea081bb3d2c8228acda
volosgoto <volosgoto@yahoo.com>
2019-05-15 14:28:06 +0300

    from chid to child

---
+++ lesson28/intermetiate/src/Components/User/Friends.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="user_friends">
+    <h2>User friends:</h2>
+    <ul>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: ["Sara", "Bob", "Mikre"]
+    };
+  }
+};
+</script>
+
+
+<style >
+.user_friends {
+  border: 1px solid #2196f3;
+}
+</style>
